78d5e56e89c0e18e8ac528a0f2709d2fb6a2cab0
Effi <effoeffi@users.noreply.github.com>
2017-03-01 14:50:21 +0200

    lambda and circle build parameters added;

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var request = require('request');
 
 exports.handler = function (event, context) {
-    console.log(process.env.TOKEN);
+    request.post({
+		headers: {'content-type':'application/json'},
+		url: process.env.CIRCLE_BUILD_URL,
+		json: {"build_parameters":{"ANSIBLE_ACTION": process.env.ANSIBLE_ACTION}}
+	}, function (error, response, body) {
+		console.log('Error:', error);
+		console.log('Status Code:', response && response.statusCode);
+		console.log('Body:', body);
+	});
 };
